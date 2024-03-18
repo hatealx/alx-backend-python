9e6e4e1c50dbf07fff803efc2e96df1b13119533
Abel <aatsou12@gmail.com>
2024-03-18 07:00:43 -0700

    doen

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A type-annotated function concat that takes a string str1 and a string str2
+as arguments anns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenates str1 and str2 then returns the concatenated string"""
+    return str1 + str2
