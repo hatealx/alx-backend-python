d192e91b094827e1545134359e8dc3c83ad19863
Abel <aatsou12@gmail.com>
2024-03-18 05:02:02 -0700

    don

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+import asyncio
+import random
 
+
+async def wait_random(max_delay=10):
+    """my coroutine fuction"""
+    delay = random.uniform(0, max_delay)  # Generate a random delay
+    await asyncio.sleep(delay)  # Wait for the random delay
+    return delay
+
+
+async def main():
+    """ my main function"""
+    random_delay = await wait_random()
+
+asyncio.run(main())
