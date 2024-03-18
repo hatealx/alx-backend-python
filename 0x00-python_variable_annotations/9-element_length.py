db7b08b8637d3f9a8846c00cb4727615c3b63854
Abel <aatsou12@gmail.com>
2024-03-18 09:09:19 -0700

    done

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+i#!/usr/bin/env python3
+"""A variable length:
+Let's duck type an iterable object"""
+import typing
+
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> (
+        typing.List)[typing.Tuple[typing.Sequence, int]]:
+    """Returns the length of an iterable object"""
+    return [(i, len(i)) for i in lst]
