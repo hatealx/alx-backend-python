08b2f4514e48c4da258a51ad2582141a48512a6e
Abel <aatsou12@gmail.com>
2024-03-21 09:52:25 -0700

    done

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,8 +3,7 @@
 
 from typing import List
 
-
-''' Import wait_random from the previous python file that you’ve
+""" Import wait_random from the previous python file that you’ve
     written and write an async routine called wait_n that takes
     in 2 int arguments (in this order): n and max_delay. You will
     spawn wait_random n times with the specified max_delay.
@@ -12,11 +11,11 @@ from typing import List
     wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order without using
     sort() because of concurrency.
-'''
+"""
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    ''' returns a sorted list of float numbers gotten randomly'''
+    """ returns a sorted list of float numbers gotten randomly"""
     wait_random = __import__('0-basic_async_syntax').wait_random
 
     delay_list = []
@@ -26,4 +25,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         delay_list.append(await wait_random(max_delay))
         i += 1
 
-    return sorted(delay_list)
+    return sorted(delay_listi)
